f253091fa2b670fbe1f677a0dd80eaac4b1eed49
Lilian Aasa <91128698+AALilian@users.noreply.github.com>
2024-04-01 17:34:20 +0300

    4

--- 4_summa_asynkronisilla_funktiokutsuilla/main.cpp
+++ 4_summa_asynkronisilla_funktiokutsuilla/main.cpp
@@ -4,3 +4,48 @@ osansa summa. Lopuksi yhdist‰ kaikkien s‰ikeiden tulokset saadaksesi kokonaissum
 luo s‰ikeet ja odottaa niiden p‰‰ttymist‰. Varmista, ett‰ tulos on oikein.
 Luo s‰ikeet std::thread-luokan avulla.
 K‰yt‰ toteutukseen asynkronisia funktiokutsuja !!!!!*/
+
+#include <iostream>
+#include <future>
+#include <vector>
+#include <thread>
+
+int count_elements(const std::vector<int>& vector, int start, int end)
+{
+	int sum = 0;
+	for (int i = start; i < end; ++i)
+	{
+		sum += vector[i];
+	}
+
+	return sum;
+}
+
+int main()
+{
+	int sum = 0;
+	int total_elements = 12000;
+	int threads = 5;
+
+	std::vector<int> large_array(total_elements, 2); //kaikki arvot on 2
+
+	int chunk = total_elements / threads; // 12000 / 5 = 2400 
+	std::vector<std::future<int>> futures(threads);
+
+	for (int i = 0; i < threads; ++i)
+	{
+		int start = i * chunk;
+		int end = (i == threads - 1) ? total_elements : (i + 1) * chunk;
+		futures[i] = std::async(std::launch::async, count_elements, std::ref(large_array), start, end);
+	}
+
+	int total = 0;
+	for (std::future<int>& future : futures)
+	{
+		total += future.get();
+	}
+
+	std::cout << "Vektorin summa: " << total;
+
+	return 0;
+}
