966c50312a1ac0c665e166dd79fd346ce8d51616
Lilian Aasa <91128698+AALilian@users.noreply.github.com>
2024-04-01 16:52:34 +0300

    git sekoiluu

---
+++ 4_summa_asynkronisilla_funktiokutsuilla/main.cpp
@@ -0,0 +1,6 @@
+/*Kirjoita ohjelma, joka laskee (suuren) taulukon (esim. vektorin) elementtien summan k‰ytt‰en useita
+s‰ikeit‰. Jaa taulukko yht‰ suuriin osiin, anna jokainen osa eri s‰ikeelle ja anna niiden laskea oman
+osansa summa. Lopuksi yhdist‰ kaikkien s‰ikeiden tulokset saadaksesi kokonaissumman. P‰‰ohjelma
+luo s‰ikeet ja odottaa niiden p‰‰ttymist‰. Varmista, ett‰ tulos on oikein.
+Luo s‰ikeet std::thread-luokan avulla.
+K‰yt‰ toteutukseen asynkronisia funktiokutsuja !!!!!*/
